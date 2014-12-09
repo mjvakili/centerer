f0abd998a107b87f9de8221e9611216580623d94
MJ <mv1003@nyu.edu>
2014-12-09 14:38:45 -0500

    Update poly.py

--- src/poly.py
+++ src/poly.py
@@ -12,9 +12,8 @@ factor = cho_factor(ATA, overwrite_a=True)
 
 def fit_3x3(img , sigma):
     a, b, c, d, e, f = cho_solve(factor, np.dot(AT, img.flatten()))
-    a = a - (sigma)/8       #equivalent to adding sigma/4 to the diagonal elements of the curvature matrix!
-    b = b - (sigma)/8
-    m = 1. / (4 * a * b - c*c)     
+  
+    m = 1. / (4 * a * b - c*c + sigma /9.)
     x = (c * e - 2 * b * d) * m
     y = (c * d - 2 * a * e) * m
     return x, y
@@ -27,7 +26,7 @@ def find_centroid(img , sigma):
       ox, oy = fit_3x3(img[xi-1:xi+2, yi-1:yi+2] , sigma)
     else:
       ox , oy = 0. , 0.
-    return xi + ox + .5 , yi + ox + .5
+    return xi + ox + .5 , yi + oy + .5
 
 
 if __name__ == "__main__":
