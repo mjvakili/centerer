a2c9ca46e937dd434300d3a22e18ac9388faaf25
MJ <mv1003@nyu.edu>
2014-12-10 14:50:21 -0500

    remove e

--- src/profile.py
+++ src/profile.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def makeMoffat(size , FWHM =3 , b = 2.5 , e = 0 , center = None ):
+def makeMoffat(size , fwhm =3 , b = 2.5  , center = None):
 
     #x = np.arange(0, size, 1, float)
     #y = x[:,np.newaxis]
@@ -14,10 +14,10 @@ def makeMoffat(size , FWHM =3 , b = 2.5 , e = 0 , center = None ):
         x0 = center[0]
         y0 = center[1]
     
-    a = FWHM/(2.*np.sqrt(2**(1./b) - 1.))
-    r = ((x-x0)**2. + ((y-y0)**2.)*(1. + np.abs(e))**2./(1. - np.abs(e))**2.)**.5
+    a = fwhm/(2.*(2**(1./b)-1)**.5)
+    r = ((x-x0)**2./a**2. + (y-y0)**2./a**2.)**.5
     
-    return ((b - 1.)*(1. + (r/a)**2.)**(-1.*b))/(np.pi*(a**2.))
+    return ((b - 1.)*(1. + r**2.)**(-1.*b))/(np.pi*(a**2.))
 
 def makeGaussian(size , FWHM =3 , e = 0 , center = None):
 
