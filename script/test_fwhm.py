f6dc333cbccbbb9a8c4279106a96969dc1418805
MJ <mv1003@nyu.edu>
2014-12-10 20:20:09 -0500

    Update test_fwhm.py

--- script/test_fwhm.py
+++ script/test_fwhm.py
@@ -4,15 +4,14 @@
 
 """
 Goal: This code tests centroiding of a set of 
-normalized moffat profiles with a given SNR and beta
-in a given range of full width at half maxima,
-using four different models:
+normalized moffat profiles with a given FWHM and beta
+in a given range of signal-to-noise-ratios,
+using three different models:
 SDSS method, Polynomial method, 
-and round PSF fitting method, 
-and elliptical PSF fitting.
+and PSF fitting method.
 
-Dependencies: numpy
-              
+Dependencies: numpy, profile.py,
+              poly.py, sdss.py
 Author: Mohammadjavad Vakili (July and August 2014)
 """
 
@@ -30,20 +29,19 @@ if __name__ == '__main__':
 
     o.add_option('-m', '--method', dest='method', default='poly',
         help='Set the centroiding method, polynomial or sdss or fitting, or efitting, default: polynomial')
-    o.add_option('-s', '--snr', dest='snr', default= 150, type='float',
-        help='SNR, default: 150')
+    o.add_option('-f', '--fwhm', dest='fwhm', default= 2., type='float',
+        help='FWHM of the moffat profile, default: 2')
     o.add_option('-b', '--beta', dest='beta', default= 2.5, type='float',
         help='Dimensionless parameter of Moffat profile, default: 2.5')
     
-    o.add_option('--fmin', dest='fmin', default=2., type='float',
-        help='Minimum fwhm in experiment, default: 2.')
-    o.add_option('--fmax', dest='fmax', default=5.6, type='float',
-        help='Maximum fwhm in experiment, default: 5.6')
+    o.add_option('--smin', dest='smin', default=5., type='float',
+        help='Minimum S/N in experiment, default: 10.0')
+    o.add_option('--smax', dest='smax', default=150, type='float',
+        help='Maximum S/N in experiment, default: 150.0')
     o.add_option('--size', dest='size', default=17, type='int',
         help='size of the postage_stamp')
     o.add_option('--sample', dest='sample', default=20000, type='int',
         help='number of stars in experiment')
-
     opts, args = o.parse_args(sys.argv[1:])
 
 
@@ -52,14 +50,14 @@ if __name__ == '__main__':
     if not (opts.size is None):
         size = opts.size
     
-    if not (opts.snr is None):
-        snr = opts.snr
+    if not (opts.fwhm is None):
+        fwhm = opts.fwhm
 
-    if not (opts.fmin is None):
-        fmin = opts.fmin
+    if not (opts.smin is None):
+        smin = opts.smin
 
-    if not (opts.fmax is None):
-        fmax = opts.fmax
+    if not (opts.smax is None):
+        smax = opts.smax
 
     if not (opts.sample is None):
         sample = opts.sample
@@ -68,164 +66,177 @@ if __name__ == '__main__':
         beta = opts.beta
     
     
-    xmin , xmax = size/2 , size/2 + 1
-    ymin , ymax = size/2 , size/2 + 1
+    xmin , xmax = size/2   , size/2 + 1
+    ymin , ymax = size/2   , size/2 + 1
     
-    #f = np.random.uniform(fmin, fmax, sample)
-    f = np.loadtxt("f_snr5.txt")
-    sigma = 0.478/(f*snr)
+    #sigmamin = .478/(fwhm*smax*((1.-0)/(1.+0))**.5)
+    #sigmamax = .478/(fwhm*smin*((1.-0)/(1.+0))**.5)
     
-    #xx = np.random.uniform(xmin, xmax, sample)
-    #yy = np.random.uniform(ymin, ymax, sample)
-
-    #np.savetxt("xc_snr5.txt"  , np.array(xx)  ,fmt='%.12f')
-    xx = np.loadtxt("xc_snr5.txt")
-    #np.savetxt("yc_snr5.txt"  , np.array(yy)  ,fmt='%.12f')
-    yy = np.loadtxt("yc_snr5.txt")   
-    #np.savetxt("f_snr5.txt"   ,  np.array(f)  ,fmt='%.12f')
-    """F = h5py.File('data_snr5.h5' , 'r')
-    DATA = F["MyDataset"][:]"""
+    #snr = 10.**(np.random.uniform(np.log10(smin), np.log10(smax), sample))    #creating S/Ns
+    #np.savetxt("snr.txt" , np.array(snr)  ,fmt='%.12f')                 #saving S/Ns
+    snr = np.loadtxt("snr.txt")                                            #loading S/Ns
+    
+    sigma = .478/(fwhm*snr)                                                   #always stays here
+    
+    #xx = np.random.uniform(xmin, xmax, sample)                                #creating xs for all experiments
+    #np.savetxt("xc_f5.txt"  , np.array(xx)  ,fmt='%.12f')                   #saving xs for all experiments
+    xx = np.loadtxt("xc_f5.txt")                                            #loading xs
+    
+    #yy = np.random.uniform(ymin, ymax, sample)                                #creating ys for all
+    #np.savetxt("yc_f5.txt"  , np.array(yy)  ,fmt='%.12f')                   #saving ys for all
+    yy = np.loadtxt("yc_f5.txt")                                            #loading ys
+    
+    F = h5py.File('data_f%d.h5'%(10*fwhm) , 'r')                   #loading file
+    DATA = F["MyDataset"][:]                                    #loading file
+    #print DATA
+    
     if opts.method.startswith('poly'):
-       cent = []
-       erx  = []
-       ery  = []
-       flag = []
-       fwhm = []
-       A = centererr.poly.design(2)
 
-       for i in range(sample):
-          
-          xc =  np.array([xx[i],yy[i]])          
-          data = centererr.profile.makeMoffat(size , f[i] , beta , 0.0 , xc)
-          data += np.random.normal(0, sigma[i] , data.shape)
-          
-          xs = centererr.poly.poly_centroid(data , A)
-          #cen = centererr.poly.BP(data)
-          #cent.append(cen[0])
-          erx.append((xs[0] - xc[0])**2.)
-          ery.append((xs[1] - xc[1])**2.)
-          fwhm.append(f[i])
-          if ((xs[0]>.5)|(xs[0]<-.5)|(xs[1]>.5)|(xs[1]<-.5)|(xs[0].imag!=0.)|(xs[1].imag!=0.)):
-          
-            flag.append(i)
-       #np.savetxt("center1_s40_00.txt" , np.array(cent))
-       np.savetxt("erx1_s5_00s.txt"  , np.array(erx)  ,fmt='%.12f')
-       np.savetxt("ery1_s5_00s.txt"  , np.array(ery)  ,fmt='%.12f')
-       np.savetxt("flag1_s5_00s.txt" , np.array(flag) , fmt = '%.12f')
-       np.savetxt("fwhm1_s5_00s.txt" , np.array(fwhm) , fmt = '%.12f')
-
-    
-    elif opts.method.startswith('spoly'):
-       #cent = []
        erx = []
        ery = []
        flag = []
-       fwhm = []
-       A = centererr.spoly.design(2)
-       F = h5py.File('data_snr40.h5')
-       dset = F.create_dataset("MyDataset" , (100000 , 17 , 17) , 'f8')
-       
+       #noise = []
+       #cent = []
+       #A = centererr.poly.design(2)
+
        for i in range(sample):
-        
-        
-		         
-        xc =  np.array([xx[i],yy[i]])          
-        data = centererr.profile.makeMoffat(size , f[i] , beta , 0.0 , xc)
-        
-        data += np.random.normal(0, sigma[i] , data.shape)
-        #data = DATA[i]                                       #loading
-        bp = np.where(data==data.max())
-        dset[i] = data                                        #saving 
-        
-        if ([bp[1][0] , bp[0][0]] == [8,8]):
           
-          xs = centererr.spoly.spoly_centroid(data , A , 1.2 , sigma[i])
+          xc    =  np.array([xx[i],yy[i]])          
+          data = DATA[i]
+              
+          xs = centererr.polynomial.find_centroid(data , sigma[i])
+          #cen = centererr.poly.BP(data)
+          #cent.append(cen[0])
+          erx.append((xs[1] - xc[0])**2.)
+          ery.append((xs[0] - xc[1])**2.)
+
+          #if ((xs[0]>.5)|(xs[0]<-.5)|(xs[1]>.5)|(xs[1]<-.5)|(xs[0].imag!=0.)|(xs[1].imag!=0.)):
           
-          erx.append((xs[0] - xc[0])**2.)
-          ery.append((xs[1] - xc[1])**2.)
-          fwhm.append(f[i])
-          if ((xs[0]>.5)|(xs[0]<-.5)|(xs[1]>.5)|(xs[1]<-.5)|(xs[0].imag!=0.)|(xs[1].imag!=0.)):
+          #  flag.append(i)
+       #np.savetxt("center1_20_00.txt" , np.array(cent) , fmt='%.12f')
+       np.savetxt("erx1_f%d_00r.txt"%(10*fwhm) , np.array(erx) ,fmt='%.12f')
+       np.savetxt("ery1_f%d_00r.txt"%(10*fwhm) , np.array(ery) ,fmt='%.12f')
+       #np.savetxt("flag1_f%d_00r.txt"%(fwhm) , np.array(flag) , fmt = '%.12f')
+       #np.savetxt("noise1_80_00.txt" , np.array(noise) , fmt = '%.12f')
+       F.close()
+    elif opts.method.startswith('spoly'):
+   
+ 
+
+       erx = []
+       ery = []
+       #flag = []
+       #noise = []
+       #cent = []
+       #F = h5py.File('data_f56.h5')                                                   #saving
+       #dset = F.create_dataset("MyDataset" , (100000 , 17 , 17) , 'f8')               #saving 
+       #A = centererr.spoly.design(2)
+
+       for i in range(sample):
           
-            flag.append(i)
-       F.close() 
-       #np.savetxt("center5_s40_00.txt" , np.array(cent))
-       np.savetxt("erx5_s5_00tb.txt"  , np.array(erx)  , fmt='%.12f')
-       np.savetxt("ery5_s5_00tb.txt"  , np.array(ery)  , fmt='%.12f')
-       np.savetxt("flag5_s5_00tb.txt" , np.array(flag) , fmt = '%.12f')
-       np.savetxt("fwhm5_s5_00tb.txt" , np.array(fwhm) , fmt = '%.12f')
+          xc =  np.array([xx[i],yy[i]])  
+          data  = DATA[i]                                                      #loading        
+          #data = centererr.profile.makeMoffat(size , fwhm , beta , xc)  #saving
+          #data += np.random.normal(0, sigma[i] , data.shape)                  #saving               
+          #dset[i] = data                                                      #saving
+          xs = centererr.spolynom.find_centroid(data , 1.2, sigma[i])
+          #ce = centererr.spoly.BP(data , 2.)
+          #cent.append(ce[0])
+          erx.append((xs[1] - xc[0])**2.) 
+          ery.append((xs[0] - xc[1])**2.)
+          #noise.append(snr[i])
+          #if ((xs[0]>.5)|(xs[0]<-.5)|(xs[1]>.5)|(xs[1]<-.5)|(xs[0].imag!=0.)|(xs[1].imag!=0.)):
+          
+          #  flag.append(i)
+       F.close()
+       #np.savetxt("center5_60_00.txt" , np.array(cent))
+       np.savetxt("erx5_f%d_00r.txt"%(10*fwhm)  ,   np.array(erx)  ,fmt='%.12f')# ,fmt='%.8f')
+       np.savetxt("ery5_f%d_00r.txt"%(10*fwhm)  ,   np.array(ery)  ,fmt='%.12f')# ,fmt='%.8f')
+       #np.savetxt("flag55_f%d_00r.txt"%(fwhm) ,   np.array(flag) ,fmt='%.12f')# , fmt = '%.8f')
+       #np.savetxt("noise5_20_00.txt",   np.array(noise),fmt='%.12f')# , fmt = '%.8f')
+
+
 
     elif opts.method.startswith('sdss'):
 
        erx = []
        ery = []
-       flag = []
-       fwhm = []
        #cent = []
+       #flag = []
+       #noise = []
+       
 
        for i in range(sample):
+          data = DATA[i]  #loading the data
+          #print data
+          xc =  np.array([xx[i],yy[i]])                      # true centroid
           
-          xc =  np.array([xx[i],yy[i]])                  # true centroid
-          data = centererr.profile.makeMoffat(size , f[i], beta  , 0.0 , xc)
-          data += np.random.normal(0, sigma[i] , data.shape)
-          #cen = centererr.sdss.BP(data , 2.)
-          xs = centererr.sdss.sdss_centroid(data , 1.2 , sigma[i])
-          #cent.append(cen[0])
+          #data = centererr.profile.makeMoffat(size , fwhm, beta  , 0. , xc)
+          #data += np.random.normal(0, sigma[i] , data.shape)
+          #cen = centererr.sdss.BP(data , 2.)                 # have to choose the width of the kernel here       
+          xs = centererr.sdss.sdss_centroid(data , 1.2 , sigma[i])  #have to choose the width of the kernel here
+          
           erx.append((xs[0] - xc[0])**2.)
           ery.append((xs[1] - xc[1])**2.)
-          fwhm.append(f[i])
-          if ((xs[0]>.5)|(xs[0]<-.5)|(xs[1]>.5)|(xs[1]<-.5)|(xs[0].imag!=0.)|(xs[1].imag!=0.)):
+          #noise.append(snr[i])
+          #cent.append(cen[0])
+          #if ((xs[0]>.5)|(xs[0]<-.5)|(xs[1]>.5)|(xs[1]<-.5)|(xs[0].imag!=0.)|(xs[1].imag!=0.)):
           
-            flag.append(i)
-       #np.savetxt("center2_s5_00.txt", np.array(cent))   
-       np.savetxt("erx2_s20_00.txt"   , np.array(erx)  , fmt='%.12f')
-       np.savetxt("ery2_s20_00.txt"   , np.array(ery)  , fmt='%.12f')
-       np.savetxt("flag2_s20_00.txt"  , np.array(flag) , fmt = '%.12f')
-       np.savetxt("fwhm2_s20_00.txt"  , np.array(fwhm) , fmt = '%.12f')
-       
+          #  flag.append(i)
+       #np.savetxt("center2_20_00.txt" , np.array(cent))   
+       np.savetxt("erx2_f%d_00r.txt"%(10*fwhm) , np.array(erx)     ,fmt='%.12f')
+       np.savetxt("ery2_f%d_00r.txt"%(10*fwhm) , np.array(ery)     ,fmt='%.12f')
+       #np.savetxt("flag2_80_00.txt" , np.array(flag)   ,fmt='%.12f')
+       #np.savetxt("noise2_80_00.txt" , np.array(noise) ,fmt='%.12f')
+       F.close()
     elif opts.method.startswith('fitting'):
+       
+       erx = []
+       ery = [] 
+       #flag = []
+       #noise = []
+       
+
+       for i in range(sample):
+          data = DATA[i]
+          #print data
+          xc =  np.array([xx[i],yy[i]])                  # true centroid
+          #data = centererr.profile.makeMoffat(size , fwhm , beta ,xc)
+          #data += np.random.normal(0, sigma[i] , data.shape)
+          
+          xs = centererr.fitting.fitting_centroid(data , sigma[i] , fwhm , 2.5)
+          #print xs
+          #print xc
+          erx.append((xs[0]  - xc[0])**2.)
+          ery.append((xs[1]  - xc[1])**2.)
+          #noise.append(snr[i])
+          
+       np.savetxt("erx3_f%d_00r.txt"%(10*fwhm)   , np.array(erx)   , fmt='%.12f')
+       np.savetxt("ery3_f%d_00r.txt"%(10*fwhm)  , np.array(ery)   , fmt='%.12f')
+       #np.savetxt("noise3_60_00.txt" , np.array(noise) , fmt = '%.12f')
+       #np.savetxt("flag3_60_00.txt"  , np.array(flag)  , fmt = '%.12f')
+       F.close()
+    elif opts.method.startswith('efitting'):
        
        erx = []
        ery = []
        flag = []
-       fwhm = []
+       #noise = []
        
 
        for i in range(sample):
-          
+          data = DATA[i]
           xc =  np.array([xx[i],yy[i]])                  # true centroid
-          data = centererr.profile.makeMoffat(size , f[i] , beta , 0.0 , xc)
-          data += np.random.normal(0, sigma[i] , data.shape)
+          #data = centererr.profile.makeMoffat(size , fwhm , beta , 0.2 , xc)
+          #data += np.random.normal(0, sigma[i] , data.shape)
           
-          xs = centererr.fitting.fitting_centroid(data , sigma[i] , f[i] , 2.5)
-          
-          erx.append((xs[0]  - xc[0])**2.)
+          xs = centererr.efitting.fitting_centroid(data , sigma[i] , fwhm , 2.5)
+          print xs[0]
+          erx.append((xs[0]  - xc[0])**2.) 
           ery.append((xs[1]  - xc[1])**2.)
-          fwhm.append(f[i])
+          #noise.append(sigma[i])
           
-       np.savetxt("erx3_s10_00.txt"   , np.array(erx)   , fmt = '%.12f')
-       np.savetxt("ery3_s10_00.txt"   , np.array(ery)   , fmt = '%.12f')
-       np.savetxt("flag3_s10_00.txt" , np.array(flag) , fmt = '%.12f')
-       np.savetxt("fwhm3_s10_00.txt" , np.array(fwhm) , fmt = '%.12f')
-       
-    
-    elif opts.method.startswith('efitting'):
-       
-       er = []
-       flag = []
-       fwhm = []
-       
-
-       for i in range(sample):
-          
-          xc =  np.array([xx[i],yy[i]])                  # true centroid
-          data = centererr.profile.makeMoffat(size , f[i] , beta , 0.2 , xc)
-          data += np.random.normal(0, sigma[i] , data.shape)
-          
-          xs = centererr.efitting.fitting_centroid(data)
-          
-          er.append(((xs[0]  - xc[0])**2. + (xs[1]  -xc[1])**2.)**.5)
-          fwhm.append(f[i])
-          
-       np.savetxt("er4_s10_02.txt" , np.array(er) ,fmt='%.8f')
-       np.savetxt("flag4_s10_02.txt" , np.array(flag) , fmt = '%.8f')
-       np.savetxt("fwhm4_s10_02.txt" , np.array(fwhm) , fmt = '%.8f')
+       np.savetxt("erx4_56_00r.txt" , np.array(erx) ,fmt='%.8f')
+       np.savetxt("ery4_56_00r.txt" , np.array(ery) ,fmt='%.8f')
+       #np.savetxt("noise4_56_02.txt" , np.array(noise) , fmt = '%.8f')
+       #np.savetxt("flag4_56_02.txt" , np.array(flag) , fmt = '%.8f')
