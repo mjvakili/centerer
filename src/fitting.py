1752fe5013fe6d38a318bdc2d137bd502d4c7dd4
MJ <mv1003@nyu.edu>
2015-01-26 11:43:46 -0500

    Update fitting.py

--- src/fitting.py
+++ src/fitting.py
@@ -7,7 +7,7 @@ def Moffat(FWHM , b , flux , x0 , y0 , x , y ):
     r = ((x-x0)**2. +(y-y0)**2.)**.5
     return (flux*(b - 1.)*(1. + (r/a)**2.)**(-1.*b))/(np.pi*a**2.)
 
-def lnlike(theta , I , sigma , FWHM , b , x , y):
+def lnlike(theta , I , flux , sigma , FWHM , b , x , y):
 
     flux , x0 , y0 = theta
     model = Moffat(FWHM , b , flux , x0 , y0 , x , y )
@@ -17,22 +17,22 @@ def lnlike(theta , I , sigma , FWHM , b , x , y):
 
 
 
-def fitting_centroid(I , sigma , FWHM , b):
+def fitting_centroid(I , flux , sigma , FWHM , b):
     size = I.shape[0]
     x = np.linspace(0.5, size - .5, size)
     y = x[:,np.newaxis]
     x0_true = I.shape[0]/2.
     y0_true = I.shape[0]/2.
-    flux0_true = 1.
+    #flux0_true = 1.
     size = I.shape[0]
     x = np.linspace(0.5, size - .5, size)
     y = x[:,np.newaxis]
     nll = lambda *args: -lnlike(*args)
-    results = op.fmin(nll , [flux0_true , x0_true , y0_true] , args = (I , sigma , FWHM , b , x , y) , disp = False)
+    results = op.fmin(nll , [x0_true , y0_true] , args = (I , flux , sigma , FWHM , b , x , y) , disp = False)
  
     #flux0_ml , x0_ml , y0_ml = results["x"]
 
-    return results[1] , results[2]
+    return results[0] , results[1]
 
 if __name__ == "__main__":
     print 'fitting main'
